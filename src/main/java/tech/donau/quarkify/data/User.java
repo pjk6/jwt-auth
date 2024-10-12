accc0269d97aa2fb8f92b3fdb42bf1785ff39304
Peter Keereweer <pj.keereweer@belastingdienst.nl>
2024-10-12 20:50:26 +0200

    Remove H2 database usage; use quarkus dev service with postgres DB; move to java 21

--- src/main/java/tech/donau/quarkify/data/User.java
+++ src/main/java/tech/donau/quarkify/data/User.java
@@ -1,13 +1,36 @@
 package tech.donau.quarkify.data;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+    import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 
 @Entity
 @Table(name="Users")
+//@UserDefinition()
 public class User extends PanacheEntity {
+    //@Username
     public String login;
+    //@Password
+    public String password;
     public String email;
-    public String password; // Use e.g Bcrypt to encrypt password, don't store it as plain text :)
+
+    /**
+     * Adds a new user to the database
+     * @param login the username
+     * @param password the unencrypted password (it is encrypted with bcrypt)
+     * @param role the comma-separated roles
+     */
+//    public static void add(String username, String password, String role) {
+//        User user = new User();
+//        user.username = username;
+//        user.password = BcryptUtil.bcryptHash(password);
+//        user.role = role;
+//        user.persist();
+//    }
 }
