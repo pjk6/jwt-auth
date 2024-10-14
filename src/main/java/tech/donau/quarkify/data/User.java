b262b8b5d9e4e7b975926308044092ee09bc19dc
Peter Keereweer <pj.keereweer@belastingdienst.nl>
2024-10-14 12:48:53 +0200

    Store encrypted passwords using Bcrypt

--- src/main/java/tech/donau/quarkify/data/User.java
+++ src/main/java/tech/donau/quarkify/data/User.java
@@ -1,7 +1,6 @@
 package tech.donau.quarkify.data;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
@@ -12,25 +11,9 @@ import jakarta.persistence.Entity;
 
 @Entity
 @Table(name="Users")
-//@UserDefinition()
 public class User extends PanacheEntity {
-    //@Username
     public String login;
-    //@Password
     public String password;
+    public String roles;
     public String email;
-
-    /**
-     * Adds a new user to the database
-     * @param login the username
-     * @param password the unencrypted password (it is encrypted with bcrypt)
-     * @param role the comma-separated roles
-     */
-//    public static void add(String username, String password, String role) {
-//        User user = new User();
-//        user.username = username;
-//        user.password = BcryptUtil.bcryptHash(password);
-//        user.role = role;
-//        user.persist();
-//    }
 }
--- src/main/java/tech/donau/quarkify/user/UserResource.java
+++ src/main/java/tech/donau/quarkify/user/UserResource.java
@@ -1,7 +1,10 @@
 package tech.donau.quarkify.user;
 
 import tech.donau.quarkify.data.User;
+import tech.donau.quarkify.security.Roles;
 import tech.donau.quarkify.security.TokenService;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
 
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -9,10 +12,14 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import org.jboss.logging.Logger;
+
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+    private static final Logger LOG = Logger.getLogger(UserResource.class);
+
     @Inject
     TokenService service;
 
@@ -20,17 +27,36 @@ public class UserResource {
     @Path("/register")
     @Transactional
     public User register(User user) {
-        user.persist(); //super simplified registration, no checks of uniqueness
+        // Encrypt password using Bcrypt
+        user.password = BcryptUtil.bcryptHash(user.password);
+
+        // Add default USER role to the registered user
+        user.roles = Roles.USER;
+
+        // Check if the user already exists
+        if(User.find("login", user.login).firstResult() != null){
+            throw new WebApplicationException(Response.status(409)
+                    .entity("User already exists!")
+                    .build());
+        }
+
+        // Save the user into the database
+        user.persist();
+
         return user;
     }
 
     @GET
     @Path("/login")
-    public String login(@QueryParam("login")String login, @QueryParam("password") String password) {
-        User existingUser = User.find("login", login).firstResult();
-        if(existingUser == null || !existingUser.password.equals(password)) {
-            throw new WebApplicationException(Response.status(404).entity("No user found or password is incorrect").build());
+    public String login(@QueryParam("login") String login, @QueryParam("password") String password) {
+        User existingUser = User.find("login", login)
+                                .firstResult();
+        if(existingUser == null || !BcryptUtil.matches(password, existingUser.password)) {
+                throw new WebApplicationException(Response.status(404)
+                        .entity("No user found or wrong password")
+                        .build());
         }
+
         return service.generateUserToken(existingUser.email, password);
     }
 }
